7976329e5ff22378751e4086448b4ed1a90fa9da
Ibaii99 <ibai.guillen@opendeusto.es>
2020-12-06 20:08:25 +0100

    Prefabs, Spawner and textures

---
+++ Assets/Easy FPS/Scripts/AnimalAI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalIA : MonoBehaviour
+{
+
+
+    public float speed = 7.5f;
+    public float maximunDistance = 100f; 
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    Vector2 rotation = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Easy FPS/Scripts/AnimalSpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalSpawner : MonoBehaviour
+{
+    public GameObject animalOriginal;
+
+    public int animalCount = 10;
+
+    public int range = 10;
+    
+    public float heigh = 0;
+
+    public int minimunDistance = 14;
+
+    private Vector3[] animals;
+
+    private void Awake()
+    {
+        animals = new Vector3[animalCount];
+        int i = 0;
+        while (i < animalCount)
+        {
+            Vector3 v = generatePositionVector();
+            if (availableSpace(v))
+            {
+                Quaternion baseAnimalRotation = animalOriginal.transform.rotation;
+                var animalGameObject = Instantiate(animalOriginal, v, baseAnimalRotation);
+                i++;
+            } 
+        }
+    }
+
+    private bool availableSpace(Vector3 vector)
+    {
+        for(int i = 0; i < animals.Length;  i++)
+        {
+            if(Vector3.Distance(animals[i], vector) < minimunDistance)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private Vector3 generatePositionVector()
+    {
+        float x = Random.Range(-range, range);
+        float z = Random.Range(-range, range);
+        return new Vector3(transform.position.x + x, heigh, transform.position.z + z);
+    }
+
+}
