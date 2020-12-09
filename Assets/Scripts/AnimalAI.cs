884750963aec419862f076eff1b69398adaeede0
Ibaii99 <ibai.guillen@opendeusto.es>
2020-12-09 21:41:32 +0100

    IA de dudosa calidad, 1iq

--- Assets/Scripts/AnimalAI.cs
+++ Assets/Scripts/AnimalAI.cs
@@ -2,25 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimalIA : MonoBehaviour
+public class AnimalAI : MonoBehaviour
 {
+    //Multiplicador de la velocidad
+    public float animalSpeed;
+
+    public CharacterController animalCharacter;
+    private Rigidbody animalRigidbody;
+    private Animator animator;
+
+    private bool isMoving;
 
 
-    public float speed = 7.5f;
-    public float maximunDistance = 100f; 
-    CharacterController characterController;
-    Vector3 moveDirection = Vector3.zero;
-    Vector2 rotation = Vector2.zero;
 
-    // Start is called before the first frame update
+    public int rotationRange;
+    private int rotation;
+
+    public float moveTime;
+    private float routineTime;
+
     void Start()
     {
-        
+        rotation = Random.Range(-rotationRange, rotationRange);
+        isMoving = false;
+        animalRigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        animalCharacter = GetComponent<CharacterController>();
+        routineTime = moveTime;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        animator.SetBool("Moving", isMoving);
+
     }
+
+    private void FixedUpdate()
+    {
+
+        routineTime -= Time.deltaTime;
+        if(routineTime >= 0)
+        {
+            isMoving = true;
+            avance();
+        }
+        else
+        {
+            isMoving = false;
+            if (routineTime <= -2)
+            {
+
+                rotate();
+                if (routineTime <= -3)
+                {
+                    routineTime = moveTime;
+                }
+            }
+        }
+    }
+
+    private void avance()
+    {
+        animalCharacter.Move(transform.forward * animalSpeed * Time.deltaTime);
+    }
+
+    public void rotate()
+    {
+        this.transform.Rotate(new Vector3(0, rotation, 0) * animalSpeed * Time.deltaTime);
+    }   
+
+
+
 }
