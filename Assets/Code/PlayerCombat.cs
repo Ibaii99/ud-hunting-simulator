75ef79aa939e4b9ea958d23abd7333324b3faca3
Sanrro10 <i.glzsr@opendeusto.es>
2020-12-13 15:23:03 +0100

    Animaciones de disparo y apuntar

---
+++ Assets/Code/PlayerCombat.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerCombat : MonoBehaviour
+{
+    public Transform rightGunBone;
+    public CharacterController controller;
+    public GameObject weapon;
+    public Camera camara;
+
+    Animator anim;
+    Animator weaponAnim;
+    int aimingHash = Animator.StringToHash("Aiming");
+    int shootHash = Animator.StringToHash("Shoot");
+    int deathHash = Animator.StringToHash("Death");
+    int health = 100;
+    bool apuntando = false;
+    private float currentFOV;
+    private float startingFOV;
+
+    private void Start()
+    {
+        startingFOV = camara.fieldOfView;
+        anim = GetComponent<Animator>();
+        print(anim.name);
+        GameObject newRightGun = (GameObject)Instantiate(weapon);
+        newRightGun.transform.parent = rightGunBone;
+        newRightGun.transform.localPosition = Vector3.zero;
+        newRightGun.transform.localRotation = Quaternion.Euler(90, 0, 0);
+        currentFOV = camara.fieldOfView;
+    }
+
+    void Update()
+    {
+        if (health <= 0)
+        {
+            anim.SetTrigger(deathHash);
+            //Finalizar la partida
+        }
+
+        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+
+        if (Input.GetKey(KeyCode.Mouse1) && (x == 0 && z == 0))
+        {
+            print("Apuntando");
+            if (apuntando == false)
+            {
+                print("Empezando a apuntar");
+                anim.SetBool(aimingHash, true);
+                apuntando = true;
+                currentFOV = startingFOV - 8;
+            }
+            
+        }
+        else
+        {
+            currentFOV = startingFOV;
+            apuntando = false;
+            anim.SetBool(aimingHash, false);
+        }
+        camara.fieldOfView = currentFOV;
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            print("Disparo");
+            anim.SetTrigger(shootHash);
+        }
+        
+
+
+    }
+}
--- Assets/Code/PlayerFPSMovement.cs
+++ Assets/Code/PlayerFPSMovement.cs
@@ -9,16 +9,18 @@ public class PlayerFPSMovement : MonoBehaviour
     public float speed = 10f;
     public float runSpeed = 20f;
     public float gravity = -10f;
-    public float jumpHeight = 5f;
+    public float jumpHeight = 2f;
 
     public Transform groundCheck;
-    public float groundDistance = 0.4f;
+    public float groundDistance = 0.001f;
     public LayerMask groundMask;
 
     Animator anim;
     int jumpHash = Animator.StringToHash("Jump");
     int moveHash = Animator.StringToHash("Move");
     int runHash = Animator.StringToHash("Run");
+    int aimingHash = Animator.StringToHash("Aiming");
+    int shootHash = Animator.StringToHash("Shoot");
 
     Vector3 velocity;
     bool isGrounded;
@@ -42,22 +44,25 @@ public class PlayerFPSMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
         if(x== 0 && z == 0) {
             anim.SetBool(moveHash, false);
+            anim.SetBool(runHash, false);
         }
         else
         {
             anim.SetBool(moveHash, true);
+            anim.SetBool(aimingHash, false);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                controller.Move(move * runSpeed * Time.deltaTime);
+                anim.SetBool(runHash, true);
+            }
+            else
+            {
+                controller.Move(move * speed * Time.deltaTime);
+                anim.SetBool(runHash, false);
+            }
         }
 
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            controller.Move(move * runSpeed * Time.deltaTime);
-            anim.SetBool(runHash, true);
-        }
-        else
-        {
-            controller.Move(move * speed * Time.deltaTime);
-            anim.SetBool(runHash, false);
-        }
+        
 
         
 
@@ -65,6 +70,8 @@ public class PlayerFPSMovement : MonoBehaviour
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
             anim.SetTrigger(jumpHash);
+            anim.SetBool(moveHash, false);
+            anim.SetBool(runHash, false);
         }
 
         velocity.y += gravity * Time.deltaTime;
